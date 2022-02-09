f59ab1c992bdb6cd95591afd4c8698caa5cb844f
ihsan-salman <ihsan.salmantr@gmail.com>
2022-02-09 22:30:04 +0100

    rename reset password templates and rework menu bar more estheticly

--- FavArche/FavArche/urls.py
+++ FavArche/FavArche/urls.py
@@ -36,13 +36,27 @@ urlpatterns = [
     url(r'^logout/$', LogoutView.as_view(), {'next_page':
         settings.LOGOUT_REDIRECT_URL}, name='logout'),
 
-    path('reset_password/', auth_views.PasswordResetView.as_view(), name="reset_password"),
-    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
-    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
+    path('reset_password/',
+         auth_views.PasswordResetView.as_view(
+            template_name='registration/password-reset/password_reset.html'),
+         name="reset_password"),
+    path('reset_password_sent/',
+         auth_views.PasswordResetDoneView.as_view(
+            template_name='registration/password-reset/password_reset_sent.html'),
+         name="password_reset_done"),
+    path('reset/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(
+            template_name='registration/password-reset/password_reset_form.html'),
+         name="password_reset_confirm"),
+    path('reset_password_complete/',
+         auth_views.PasswordResetCompleteView.as_view(
+            template_name='registration/password-reset/password_reset_done.html'),
+         name="password_reset_complete"),
 
     url(r'^my_account/', views.personal_account, name='my_account'),
-    url(r'^edit_account_information/', views.edit_account, name='edit_account'),
+    url(r'^edit_account_information/',
+        views.edit_account,
+        name='edit_account'),
     url(r'^change_password/', views.change_password, name='change_password'),
 
     url(r'^my_works/', views.personal_works, name='personal_works'),
