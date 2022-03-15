6f097ae0414f0f3f356c9eedcd504b8919f51f85
ihsan-salman <ihsan.salmantr@gmail.com>
2022-03-15 18:08:06 +0100

    1st success launch on heroku, now time to finish other stuff

--- FavArche/FavArche/settings.py
+++ FavArche/FavArche/settings.py
@@ -169,8 +169,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'ihsan.saitama@gmail.com'
-EMAIL_HOST_PASSWORD = 'salman57450'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'default')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', 'default')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -178,4 +178,3 @@ EMAIL_HOST_PASSWORD = 'salman57450'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 INTERNAL_IPS = ['127.0.0.1']
-
