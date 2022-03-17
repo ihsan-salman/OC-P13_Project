bfbfdbe3b33e89b3e170c8eb42963e644e288bfe
ihsan-salman <ihsan.salmantr@gmail.com>
2022-03-17 23:40:19 +0100

    launch

--- FavArche/FavArche/urls.py
+++ FavArche/FavArche/urls.py
@@ -38,12 +38,7 @@ urlpatterns = [
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns = [
-        url(r'^__debug__/', include(debug_toolbar.urls)),
-    ] + urlpatterns
-else:
+if not settings.DEBUG:
     import debug_toolbar
     urlpatterns = [
         url(r'^__debug__/', include(debug_toolbar.urls)),
