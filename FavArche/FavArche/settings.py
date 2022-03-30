d7b2844d8a1a91f180e663ba32079ad8777784f5
ihsan-salman <ihsan.salmantr@gmail.com>
2022-03-30 09:33:54 +0000

    delete duplicate test and add all environment variables

--- FavArche/FavArche/settings.py
+++ FavArche/FavArche/settings.py
@@ -15,12 +15,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'c@n%u@91tum=@j392g20b8znh7dqfo-v%81))gxbbmu$=dy_*)')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '192.241.138.251', 'favarche.fr']
+ALLOWED_HOSTS = ['127.0.0.1', os.environ.get('IP_ADRESS')]
 
 
 # Application definition
@@ -89,8 +89,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'favarche',
-        'USER': 'ihsan',
-        'PASSWORD': 'salman57',
+        'USER': os.environ('DATABASE_USER'),
+        'PASSWORD': os.environ('DATABASE_PASSWORD'),
         'HOST': 'localhost',
         'PORT': '5432',
     }
@@ -164,8 +164,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'ihsan.saitama@gmail.com'
-EMAIL_HOST_PASSWORD = 'igutqwwbzsmoudip'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
